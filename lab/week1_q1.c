80d954a5496c420a250dc6fcb759f23e262a2749
aashish1309 <125014044+aashish1309@users.noreply.github.com>
2023-02-13 15:21:40 +0530

    Update week1_q1.c

--- lab/week1_q1.c
+++ lab/week1_q1.c
@@ -1,1 +1,25 @@
 
+#include<stdio.h>
+int main(){
+    int n,val;
+    printf("enter the size of the array");
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++){
+        printf("enter the element");
+        scanf("%d",&arr[i]);
+    }
+    printf("enter the element to be searched in the array: ");
+    scanf("%d",&val);
+    for(int i=0;i<n;i++){
+        if(val==arr[i]){
+            printf("element found ");
+            break;
+        }
+        else{
+            printf("element not found ");
+            break;
+        }
+    }
+    return 0;
+}
